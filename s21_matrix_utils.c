87a7f8f0b25f52deb23ffb674cf0b00946c6fb2d
MyriadDao <cultivatordao@gmail.com>
2026-06-29 14:43:15 +0300

    Remove s21_print_matrix function

--- s21_matrix_utils.c
+++ s21_matrix_utils.c
@@ -1,19 +1,5 @@
 #include "s21_matrix_utils.h"
 
-void s21_print_matrix(s21_matrix* matrix)
-{
-	if (!matrix) return;
-
-	for (int i = 0; i < matrix->rows; i++)
-	{
-		for (int j = 0; j < matrix->columns; j++)
-		{
-			printf("%lf ", matrix->matrix[i][j]);
-		}
-		printf("\n");
-	}
-}
-
 int s21_matrix_from_array(int rows, int cols, int array_size, double* array, s21_matrix* dest)
 {
 	if (!rows || !cols || !array) return MATRIX_OP_ERROR;
--- s21_matrix_utils.h
+++ s21_matrix_utils.h
@@ -4,8 +4,6 @@
 #include <stdio.h>
 #include "s21_matrix.h"
 
-void s21_print_matrix(s21_matrix* matrix);
-
 int s21_matrix_from_array(int rows, int cols, int array_size, double* array, s21_matrix* dest);
 
 #endif
